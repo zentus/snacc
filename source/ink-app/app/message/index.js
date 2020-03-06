4b520e8d714fd031bda5efaf0526593dd3ee5067
zentus <zentus@cimantra.com>
2020-03-07 00:40:08 +0100

    refactor: Split out Message into its own file

---
+++ source/ink-app/app/message/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, Color } from 'ink'
+
+const Message = props => {
+  const colorProps = props.isSystem ? {
+    yellow: true
+  } : {
+    white: true
+  }
+
+  const nickString = props.isSystem ? '' : `${props.nickname}: `
+
+  return (
+		<Box key={props.key} width={"100%"} textWrap={"truncate"}>
+			<Color {...colorProps}>[{props.timestamp}] {nickString}{props.text}</Color>
+		</Box>
+	)
+}
+
+export default Message
--- source/ink-app/app/messages/index.js
+++ source/ink-app/app/messages/index.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import { Box, Color } from 'ink'
-
-const Message = props => {
-  const colorProps = props.isSystem ? {
-    yellow: true
-  } : {
-    white: true
-  }
-
-  const nickString = props.isSystem ? '' : `${props.nickname}: `
-
-  return <Color {...colorProps}>[{props.timestamp}] {nickString}{props.text}</Color>
-}
+import Message from '../message'
 
 const Messages = props => {
   const messages = [...props.messages]
@@ -19,11 +8,7 @@ const Messages = props => {
 
   return (
     <Box flexDirection="column" alignItems="flex-start" justifyContent="flex-start" {...props}>
-      {messages.map(message => (
-        <Box key={message.id} width="100%" textWrap="truncate">
-          <Message {...message}/>
-        </Box>
-      ))}
+      {messages.map(message => <Message key={message.id} {...message}/>)}
     </Box>
   )
 }
