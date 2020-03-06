c8631f4aa3c3afbc68f4c505a4845e418360a5d6
zentus <zentus@cimantra.com>
2020-03-07 00:43:14 +0100

    build: Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,11 @@
 type=$1
 tag=$(npm version ${type})
 
-echo "Releasing ${tag} ... \n"
-npm install && npm run lint && git push && git push origin "${tag}" && npm publish
+echo "* Installing" &&
+npm install &&
+echo "* Linting" &&
+npm run lint &&
+echo "* Releasing ${tag}" &&
+git push &&
+git push origin "${tag}" &&
+npm publish
