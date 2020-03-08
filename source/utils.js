1fd96f9fa8a156aeb2e1eb45c269b16bf1ad7332
zentus <zentus@cimantra.com>
2020-03-08 02:38:55 +0100

    fix: Properly validate nickname characters

--- source/utils.js
+++ source/utils.js
@@ -1,7 +1,7 @@
 export const validateNickname = nickname => {
   nickname = String(nickname).trim()
   const validLength = nickname.length > 1 && nickname.length <= 16
-  const validChars = nickname.match(/^[a-zA-Z0-9\-_]/)
+  const validChars = nickname.match(/^[a-zA-Z0-9\-_]{2,16}$/g)
   const hasAlphabetical = nickname.match(/[a-zA-Z]/)
 
   return Boolean(nickname && validLength && validChars && hasAlphabetical)
